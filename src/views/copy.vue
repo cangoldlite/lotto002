67a8224bede4b438f6868268b3d2cf05618a20a5
cangoldlite <samart.godlite@gmail.com>
2020-08-24 16:27:24 +0700

    addcopy

---
+++ src/views/copy.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>แอปนี้จัดทำโดยนาย สามารถ ทองสว่าง</h1>
+    <h2>ใครละเมิดลิชสิทธ์กรุณา</h2>
+    <b-button variant="danger">หยุด</b-button>
+  </div>
+</template>
